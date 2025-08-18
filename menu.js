0fe956e054ecbb51685618a2b66dab62c6563a90
Oleksandra Bessonova <sashaklyaus@gmail.com>
2025-08-18 14:27:14 +0300

    fixed mistakes after mentor's review

--- menu.js
+++ menu.js
@@ -3,9 +3,20 @@ const menuBtnClose = document.querySelector("[data-menu-close]");
 const mobileMenu = document.querySelector("[data-menu]");
 
 menuBtnOpen.addEventListener("click", () => {
-  mobileMenu.classList.add("is-open"); // открываем меню
+  mobileMenu.classList.add("is-open");
 });
 
 menuBtnClose.addEventListener("click", () => {
-  mobileMenu.classList.remove("is-open"); // закрываем меню
+  mobileMenu.classList.remove("is-open");
+});
+
+const menu = document.querySelector(".mobile-menu");
+const menuBtn = document.querySelector(".menu-toggle");
+const menuLinks = document.querySelectorAll(".mobile-menu__nav");
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("is-open");
+    menuBtn.classList.remove("is-active");
+  });
 });
